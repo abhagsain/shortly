4491176da0d228ca34bfcf7b8c6555f4d88c479f
Anurag Bhagsain <abhagsain@gmail.com>
2019-10-31 01:22:04 +0530

    initial sass and typography setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./sass/index.scss";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Shortly</h2>
     </div>
   );
 }
-
-export default App;
---
+++ src/sass/sass.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir abstract pages layout base components 
+cd abstract
+touch _function.scss _mixins.scss _variables.scss
+cd .. 
+cd base 
+touch _animations.scss _typography.scss _utilities.scss _base.scss
+# @import "./abstract/variables";
+# @import "./layout/header";
+# @import "./base/animations";
+# @import "./base/typography";
+# @import "./components/button";
