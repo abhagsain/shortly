55fe34de4c729aa50e3f33b40114279adf9d0581
Anurag Bhagsain <abhagsain@gmail.com>
2019-11-01 03:42:26 +0530

    styled footer call to action

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 import URLItem from "./components/URLItem";
+import CTA from "./components/CTA";
 export default function App() {
   return (
     <div>
       <Navbar />
       <div className="container">{<Hero />}</div>
-      <div className="section-2">
-        <Search error={false} />
-      </div>
+      {/* 
+        <div className="section-2">
+          <Search error={false} />
+        </div>
+        <CTA /> */}
     </div>
   );
 }
---
+++ src/components/CTA.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function CTA() {
+  return (
+    <div className="cta">
+      <h2 className="cta__heading">Boost your links today</h2>
+      <a href="#0" className="btn btn--blue btn--cta">
+        Get Started
+      </a>
+    </div>
+  );
+}
