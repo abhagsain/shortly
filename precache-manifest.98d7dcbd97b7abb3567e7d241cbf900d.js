2ec0330d8db55c365a61c19b8ce818e1228c2f4f
Anurag Bhagsain <abhagsain@gmail.com>
2019-11-05 19:08:54 +0530

    Updates

---
+++ precache-manifest.98d7dcbd97b7abb3567e7d241cbf900d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae430d50f86cd35f52ce7787bcc6c667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25064aabfa4a95e225df",
+    "url": "/static/css/main.020c994b.chunk.css"
+  },
+  {
+    "revision": "4131a2d8a79a1d975d2a",
+    "url": "/static/js/2.f96f972e.chunk.js"
+  },
+  {
+    "revision": "25064aabfa4a95e225df",
+    "url": "/static/js/main.f65cacea.chunk.js"
+  },
+  {
+    "revision": "14987df8402c3cf11240",
+    "url": "/static/js/runtime-main.01b1806d.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "996b697c33ab8772e59e3c9bec1124ea",
+    "url": "/static/media/icon-instagram.996b697c.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/static/media/illustration-working.5029d049.svg"
+  }
+]);
